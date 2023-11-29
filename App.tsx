09acbf1cd34dcc203ea159b3c888ff9b6e0131e4
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2023-11-29 23:48:05 +0900

    :bulb: Feat: 사진첩 권한 요청 기능 작업중(#1)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import WebView from 'react-native-webview';
+import PermissionUtil from './utils';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    const permisionUtil = new PermissionUtil();
+    permisionUtil.cmmReqPhotoLibraryPermission();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView source={{uri: 'https://momoo-e22ab.web.app/'}} />
