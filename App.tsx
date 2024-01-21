75e1e475bb88816809b525e9f5885db3b910eccb
kimhayeon <k.hayeon00@gmail.com>
2024-01-21 13:27:50 +0900

    :bug: 사진첩 permission 요청 코드 제거 미요청 시에도 사진첩 접근 가능

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
 import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import WebView from 'react-native-webview';
-import PermissionUtil from './utils';
 
 function App(): JSX.Element {
-  useEffect(() => {
-    const permisionUtil = new PermissionUtil();
-    permisionUtil.handlePermissionToUploadPhoto();
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
       <WebView source={{uri: 'https://momoo-e22ab.web.app/'}} />
