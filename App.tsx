dc4354035b1fa3dd67586967f4874495e55c53f5
kimhayeon <k.hayeon00@gmail.com>
2024-02-08 14:16:48 +0900

    :bulb: Feat: 기기의 뒤로가기 버튼을 누르면, 앱이 닫히지 않고 이전 페이지로 이동하는 기능

--- App.tsx
+++ App.tsx
@@ -1,11 +1,71 @@
-import React, {useEffect} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import {BackHandler} from 'react-native';
 import WebView from 'react-native-webview';
 
-function App(): JSX.Element {
+export default function App(): JSX.Element {
+  const webview = useRef<WebView<{}>>();
+  const [isCanGoBack, setIsCanGoBack] = useState(false);
+
+  const onPressHardwareBackButton = () => {
+    if (webview.current && isCanGoBack) {
+      webview.current.goBack();
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      onPressHardwareBackButton,
+    );
+
+    return () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        onPressHardwareBackButton,
+      );
+    };
+  }, [isCanGoBack]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{uri: 'https://momoo-e22ab.web.app/'}} />
+      <WebView
+        ref={node => {
+          if (node) {
+            webview.current = node;
+          }
+        }}
+        source={{
+          uri: 'https://momoo-e22ab.web.app/',
+        }}
+        injectedJavaScript={`
+        (function() {
+          function wrap(fn) {
+            return function wrapper() {
+              var res = fn.apply(this, arguments);
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+              return res;
+            }
+          }
+    
+          history.pushState = wrap(history.pushState);
+          history.replaceState = wrap(history.replaceState);
+          window.addEventListener('popstate', function() {
+            window.ReactNativeWebView.postMessage('navigationStateChange');
+          });
+        })();
+    
+        true;
+      `}
+        onMessage={({nativeEvent: state}) => {
+          if (state.data === 'navigationStateChange') {
+            setIsCanGoBack(state.canGoBack);
+          }
+        }}
+      />
     </SafeAreaView>
   );
 }
@@ -15,5 +75,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-export default App;
